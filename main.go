9bda5b6d131c3ac3a334cee20162ad0a6d73e77d
priyanshi <noxdot1134@gmail.com>
2023-02-05 10:36:00 +0530

    Added Greet Message on basis of correct answers
    Signed-off-by: priyanshi <noxdot1134@gmail.com>

--- main.go
+++ main.go
@@ -44,6 +44,7 @@ func main() {
 		case <-timer.C: // Waiting for message from the channel
 			fmt.Printf("\nYou scored %d out of %d.\n", correct, len(problems))
 			return
+
 		case answer := <-answerCh:
 			if answer == p.ans {
 				correct++
@@ -51,8 +52,12 @@ func main() {
 		}
 	}
 	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
-
+	percentage := float64(correct) / float64(len(problems)) * 100
+    if percentage > 70 {
+	fmt.Println("Congratulations! You have scored more than 70%.")
+	}
 }
+
 
 func parseLines(lines [][]string) []problem {
 	ret := make([]problem, len(lines))
